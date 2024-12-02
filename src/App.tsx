ed003fbfffb81ee9e3961293eefed338d057b79d
jojusuar <jojusuar@espol.edu.ec>
2024-12-02 13:59:27 -0500

    feat: table populate

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,105 @@
 import './App.css'
 import Grid from '@mui/material/Grid2'
+import IndicatorWeather from './components/IndicationWeather'
+import TableWeather from './components/TableWeather';
+import ControlWeather from './components/ControlWeather';
+import LineChartWeather from './components/LineChartWeather';
+import { useEffect, useState } from 'react';
+import Item from './interface/Item';
+
+interface Indicator {
+  title?: String;
+  subtitle?: String;
+  value?: String;
+}
 
 function App() {
 
+  let [indicators, setIndicators] = useState<Indicator[]>([]);
+  let [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    let request = async () => {
+      try {
+        let response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=97ba37b25701174cbb5a00019fd2efe1", { method: 'GET' });
+        if (response.ok) {
+          let parser = new DOMParser();
+          let xml: XMLDocument = parser.parseFromString(await response.text(), "application/xml");
+
+          let dataToIndicators: Indicator[] = new Array<Indicator>();
+          let dataToItems: Item[] = new Array<Item>();
+
+          let name = xml.getElementsByTagName("name")[0].innerHTML || ""
+          dataToIndicators.push({ "title": "Location", "subtitle": "City", "value": name })
+
+          let location = xml.getElementsByTagName("location")[1]
+
+          let latitude = location.getAttribute("latitude") || ""
+          dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude })
+
+          let longitude = location.getAttribute("longitude") || ""
+          dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude })
+
+          let altitude = location.getAttribute("altitude") || ""
+          dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
+          setIndicators(dataToIndicators)
+
+          let forecasts = xml.getElementsByTagName("time");
+          for( let i = 0; i < 6; i++ ) {
+            let time = forecasts[i];
+            let from = time.getAttribute("from");
+            let to = time.getAttribute("to");
+            let precipitation = time.getElementsByTagName("precipitation")[0].getAttribute("probability");
+            let humidity = time.getElementsByTagName("humidity")[0].getAttribute("value");
+            let clouds = time.getElementsByTagName("clouds")[0].getAttribute("all");
+            let item: Item = {
+              dateStart: from ? from : "",
+              dateEnd: to ? to : "",
+              precipitation: precipitation ? precipitation : "",
+              humidity: humidity ? humidity : "",
+              clouds: clouds ? clouds : ""
+            };
+            dataToItems.push(item);
+          }
+          setItems(dataToItems);
+        }
+      }
+      catch (error) { }
+    };
+    request()
+  }, []);
+  let renderIndicators = () => {
+    return indicators
+      .map(
+        (indicator, idx) => (
+          <Grid key={idx} size={{ xs: 12, xl: 3 }}>
+            <IndicatorWeather
+              title={indicator["title"]}
+              subtitle={indicator["subtitle"]}
+              value={indicator["value"]} />
+          </Grid>
+        )
+      )
+  }
   return (
     <Grid container spacing={5}>
 
       {/* Indicadores */}
-      <Grid size={{xs: 12, xl: 3}}>Elemento: Indicador 1</Grid>
-      <Grid size={{xs: 12, xl: 3}}>Elemento: Indicador 2</Grid>
-      <Grid size={{xs: 12, xl: 3}}>Elemento: Indicador 3</Grid>
-      <Grid size={{xs: 12, xl: 3}}>Elemento: Indicador 4</Grid>
+      {renderIndicators()}
 
       {/* Tabla */}
-      <Grid size={{xs: 12, xl: 8}}>Elemento: Tabla</Grid>
+      <Grid size={{ xs: 12, xl: 8 }}>
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12, xl: 3 }}>
+            <ControlWeather />
+          </Grid>
+          <Grid size={{ xs: 12, xl: 9 }}>
+            <TableWeather itemsIn={ items } />
+          </Grid>
+        </Grid></Grid>
 
       {/* Gráfico */}
-      <Grid size={{xs: 12, xl: 4}}>Elemento: Gráfico 1</Grid>
+      <Grid size={{ xs: 12, xl: 4 }}> <LineChartWeather /></Grid>
 
     </Grid>
   )
---
+++ src/components/TableWeather.tsx
@@ -0,0 +1,78 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Item from '../interface/Item';
+import { useEffect, useState } from 'react';
+
+interface MyProp {
+  itemsIn: Item[];
+}
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function BasicTable(props: MyProp) {
+  let [rows, setRows] = useState<Item[]>([]);
+  useEffect(() => {
+    setRows(props.itemsIn);
+  }, [props]);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Hora de inicio</TableCell>
+            <TableCell>Hora de fin</TableCell>
+            <TableCell>Probabilidad de precipitación</TableCell>
+            <TableCell>% Humedad</TableCell>
+            <TableCell>% Nubosidad</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, idx) => (
+            <TableRow
+              key={idx}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.dateStart}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {row.dateEnd}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {row.precipitation}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {row.humidity}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {row.clouds}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
